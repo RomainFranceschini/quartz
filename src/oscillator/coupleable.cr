752e9568ced876b83f58b772704911cb794a0f41
Romain Franceschini <franceschini.romain@gmail.com>
2016-08-09 17:09:54 +0200

    coupleable refactor

--- src/oscillator/coupleable.cr
+++ src/oscillator/coupleable.cr
@@ -10,145 +10,124 @@ module DEVS
       @output_ports ||= Hash(Name, Port).new
     end
 
-    def output_ports : Hash(Symbol | String, Port)
-      @output_ports ||= Hash(Symbol | String, Port).new
+    # Add given port to *self*.
+    def add_port(port : Port)
+      case port.mode
+      when IOMode::Input
+        input_ports[port.name] = port
+      when IOMode::Output
+        output_ports[port.name] = port
+      end
     end
 
-    @output_port_names : Array(Symbol|String)?
-    @input_port_names : Array(Symbol|String)?
-    @input_port_list : Array(Port)?
-    @output_port_list : Array(Port)?
-
-    # Adds an input port to *self*.
-    def add_input_port(*names)
-      @input_port_names = nil; @input_port_list = nil; # cache invalidation
-      add_ports(IO::Input, *names)
+    # Add given input port to *self*.
+    def add_input_port(name)
+      add_port(IOMode::Input, name)
     end
 
-    # Adds an output port to *self*.
-    def add_output_port(*names)
-      @output_port_names = nil; @output_port_list = nil; # cache invalidation
-      add_ports(IO::Output, *names)
+    # Add given output port to *self*.
+    def add_output_port(name)
+      add_port(IOMode::Output, name)
+    end
+
+    # Removes given *port* from *self*.
+    def remove_port(port : Port)
+      case port.mode
+      when IOMode::Input
+        input_ports.delete(port.name)
+      when IOMode::Output
+        output_ports.delete(port.name)
+      end
     end
 
     # Removes given input port by its *name*.
     def remove_input_port(name)
-      @input_port_names = nil; @input_port_list = nil; # cache invalidation
       input_ports.delete(name)
     end
 
     # Removes given output port by its *name*.
     def remove_output_port(name)
-      @output_port_names = nil; @output_port_list = nil; # cache invalidation
       output_ports.delete(name)
     end
 
     # Returns the list of input ports' names
     def input_port_names
-      @input_port_names ||= @input_ports.keys
+      input_ports.keys
     end
 
     # Returns the list of output ports' names
     def output_port_names
-      @output_port_names ||= @output_ports.keys
+      output_ports.keys
     end
 
     # Returns the list of input ports
     def input_port_list : Array(Port)
-      @input_port_list ||= @input_ports.values
+      input_ports.values
     end
 
     # Returns the list of output ports
     def output_port_list : Array(Port)
-      @output_port_list ||= @output_ports.values
+      output_ports.values
     end
 
     # Find the input port identified by the given *name*.
-    def input_port?(name : String|Symbol) : Port?
+    def input_port?(name : Name) : Port?
       input_ports[name]?
     end
 
     # Find the input port identified by the given *name*.
-    def input_port(name : String|Symbol) : Port
-      raise NoSuchPortError.new unless input_ports.has_key?(name)
+    def input_port(name : Name) : Port
+      raise NoSuchPortError.new("input port \"#{name}\" not found") unless input_ports.has_key?(name)
       input_ports[name]
     end
 
     # Find the output port identified by the given *name*
-    def output_port?(name : String|Symbol) : Port?
+    def output_port?(name : Name) : Port?
       output_ports[name]?
     end
 
     # Find the output port identified by the given *name*
-    def output_port(name : String|Symbol) : Port
-      raise NoSuchPortError.new unless output_ports.has_key?(name)
+    def output_port(name : Name) : Port
+      raise NoSuchPortError.new("output port \"#{name}\" not found") unless output_ports.has_key?(name)
       output_ports[name]
     end
 
     # Find or create an input port if necessary. If the given argument is nil,
-    # an input port is created with a default name. Otherwise, an attempt to
+    # an input port is created with given name. Otherwise, an attempt to
     # find the matching port is made. If the given port doesn't exists, it is
     # created with the given name.
-    protected def find_or_create_input_port_if_necessary(port : Port | Symbol | String) : Port
-      find_or_create_port_if_necessary(IO::Input, port)
+    protected def find_or_create_input_port_if_necessary(port : Name) : Port
+      find_or_create_port_if_necessary(IOMode::Input, port)
     end
 
     # Find or create an output port if necessary. If the given argument is nil,
-    # an output port is created with a default name. Otherwise, an attempt to
+    # an output port is created with given name. Otherwise, an attempt to
     # find the matching port is made. If the given port doesn't exists, it is
     # created with the given name.
-    #
-    # @param port [String, Symbol] the output port name
-    # @return [Port] the matching port or the newly created port
-    protected def find_or_create_output_port_if_necessary(port : Port | Symbol | String) : Port
-      find_or_create_port_if_necessary(IO::Output, port)
+    protected def find_or_create_output_port_if_necessary(port : Name) : Port
+      find_or_create_port_if_necessary(IOMode::Output, port)
     end
 
-    private def find_or_create_port_if_necessary(mode : IO, port : Port | Symbol | String) : Port
-      unless port.is_a?(Port)
-        name = port
-        port = if mode.output?
-          output_ports[name]?
-        else
-          input_ports[name]?
-        end
-
-        if port.nil?
-          port = add_port(mode, name)
-          DEVS.logger.warn("specified #{mode} port #{name} doesn't exist for #{self}. creating it") if DEVS.logger
-        end
+    # :nodoc:
+    private def find_or_create_port_if_necessary(mode : IOMode, port_name : Name) : Port
+      port = if mode.output?
+        output_ports[port_name]?
+      else
+        input_ports[port_name]?
       end
+
+      if port.nil?
+        port = add_port(mode, port_name)
+        DEVS.logger.warn("specified #{mode} port #{port_name} doesn't exist for #{self}. creating it") if DEVS.logger
+      end
+
       port
     end
 
-    private def add_ports(mode : IO, *names) : Array(Port)
+    # :nodoc:
+    private def add_port(mode : IOMode, port_name : Name) : Port
       case mode
-      when IO::Input
-        ports = input_ports
-      else
-        ports = output_ports
-      end
-
-      new_ports = [] of Port
-      names.each do |n|
-        if ports.has_key?(n)
-          DEVS.logger.warn(
-            "specified #{mode} port #{n} already exists for #{self}. skipping..."
-          ) if DEVS.logger
-          new_ports << ports[n]
-        else
-          p = Port.new(self, mode, n)
-          ports[n] = p
-          new_ports << p
-        end
-      end
-
-      new_ports
-    end
-
-    private def add_port(mode : IO, port_name : Symbol | String) : Port
-      case mode
-      when IO::Input
+      when IOMode::Input
         ports = input_ports
       else
         ports = output_ports
