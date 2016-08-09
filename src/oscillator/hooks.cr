1994a629a0f8b9d22786d19bf237359bc81b07b5
Romain Franceschini <franceschini.romain@gmail.com>
2016-08-09 17:05:59 +0200

    hooks bug fix added clear method

--- src/oscillator/hooks.cr
+++ src/oscillator/hooks.cr
@@ -11,24 +11,28 @@ module DEVS
     end
 
     class Notifier
-      @listeners : Hash(Symbol, Array(Proc(Symbol,Void) | Notifiable))?
+      @listeners : Hash(Symbol, Array(Proc(Symbol,Nil) | Notifiable))?
 
       def subscribe(hook : Symbol, notifiable : Notifiable)
-        @listeners ||= Hash(Symbol, Array(Proc(Symbol,Void) | Notifiable)).new { |h,k| h[k] = [] of (Proc(Symbol,Void) | Notifiable) }
+        @listeners ||= Hash(Symbol, Array(Proc(Symbol,Nil) | Notifiable)).new { |h,k| h[k] = [] of (Proc(Symbol,Nil) | Notifiable) }
         @listeners.not_nil![hook] << notifiable
       end
 
-      def subscribe(hook : Symbol, &block : Symbol -> Void)
-        @listeners ||= Hash(Symbol, Array(Proc(Symbol,Void) | Notifiable)).new { |h,k| h[k] = [] of (Proc(Symbol,Void) | Notifiable) }
+      def subscribe(hook : Symbol, &block : Symbol ->)
+        @listeners ||= Hash(Symbol, Array(Proc(Symbol,Nil) | Notifiable)).new { |h,k| h[k] = [] of (Proc(Symbol,Nil) | Notifiable) }
         @listeners.not_nil![hook] << block
       end
 
-      def unsubscribe(hook : Symbol, instance : Proc(Symbol,Void) | Notifiable)
+      def unsubscribe(hook : Symbol, instance : Proc(Symbol,Nil) | Notifiable)
         @listeners.try &.[hook].delete(instance)
       end
 
       def clear
         @listeners.try &.clear
+      end
+
+      def clear(hook : Symbol)
+        @listeners.try &.delete(hook)
       end
 
       def notify(hook : Symbol)
