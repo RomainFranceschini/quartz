6cd77023c0fd60427fb3a0072e5f599cb5d53424
Romain Franceschini <franceschini.romain@gmail.com>
2016-08-09 16:30:20 +0200

    cleanup and replace mixin name

--- src/oscillator/atomic_model.cr
+++ src/oscillator/atomic_model.cr
@@ -2,20 +2,19 @@ module DEVS
   # This class represent a DEVS atomic model.
   class AtomicModel < Model
     include Coupleable
-    include Behavior
+    include Transitions
     include Observable(TransitionObserver)
 
-    # Returns a new instance of {AtomicModel}
-    #
-    # @param name [String, Symbol] the name of the model
     def initialize(name)
       super(name)
-      #@bag = {} of Port => Type
       @bag = {} of Port => Any
     end
 
-    def inspect
-      "<#{self.class}: name=#{@name}, time=#{@time}, elapsed=#{@elapsed}>"
+    def inspect(io)
+      io << "<" << self.class.name << ": name=" << @name
+      io << ", time=" << @time.to_s(io)
+      io << ", elapsed" << @elapsed.to_s(io)
+      io << ">"
     end
 
     # Drops off an output *value* to the specified output *port*.
@@ -23,10 +22,10 @@ module DEVS
     # Raises an InvalidPortHostError if the given port doesn't belong to this
     # model.
     # Raises an InvalidPortModeError if the given port is not an output port.
-    protected def post(value : Type, port : Port)
-      raise InvalidPortHostError.new("Given port doesn't belong to this model") if port.host != self
-      raise InvalidPortModeError.new("Given port should be an output port") if port.input?
-      @bag[port] = Any.new(value)
+    protected def post(value : Type, on : Port)
+      raise InvalidPortHostError.new("Given port doesn't belong to this model") if on.host != self
+      raise InvalidPortModeError.new("Given port should be an output port") if on.input?
+      @bag[on] = Any.new(value)
     end
 
     # Drops off an output *value* to the specified output *port*.
@@ -34,18 +33,17 @@ module DEVS
     # Raises an InvalidPortHostError if the given port doesn't belong to this
     # model.
     # Raises an InvalidPortModeError if the given port is not an output port.
-    # Raises an NoSuchChildError if the given port doesn't exists.
-    protected def post(value : Type, port_name : String|Symbol)
-      post(value, self.output_port(port_name))
+    # Raises an NoSuchPortError if the given port doesn't exists.
+    protected def post(value : Type, on : Name)
+      post(value, self.output_port(on))
     end
 
-    # TODO change API to return NamedTuple ?
     # Returns outgoing messages added by the DEVS lambda (λ) function for the
     # current state.
     #
     # This method calls the DEVS lambda (λ) function
     # Note: this method should be called only by the simulator.
-    def fetch_output! : Hash(Port, Any) #Hash(Port,Type)
+    def fetch_output! : Hash(Port, Any)
       @bag.clear
       self.output
       @bag
