9291fb90e8bffef1d8429246e3f99905840ae10a
Romain Franceschini <franceschini.romain@gmail.com>
2016-08-09 17:05:13 +0200

    renamed IOMode to IO

--- src/oscillator/port.cr
+++ src/oscillator/port.cr
@@ -1,40 +1,57 @@
 module DEVS
   # A `Port` may be configured as an Input or Output IO mode.
-  enum IO
+  enum IOMode
     Input
     Output
   end
 
   # This class represents a port that belongs to a `Model` (the *host*).
   class Port
+    include Comparable(Port)
     include Observable(PortObserver)
 
-    getter :type, :name, :host
+    getter mode : IOMode
+    getter name : Name
+    getter host : Coupleable
 
     # Returns a new `Port` instance, owned by *host*
-    def initialize(@host : Coupleable, @mode : IO, @name : Symbol | String); end
+    def initialize(@host : Coupleable, @mode : IOMode, @name : Name)
+    end
 
     def add_observer(observer : PortObserver)
-      raise UnobservablePortError.new("Atomic models output ports only are observable.") if @mode == IO::Input || @host.is_a?(CoupledModel)
+      if @mode == IOMode::Input || @host.is_a?(CoupledModel)
+        raise UnobservablePortError.new("Only atomic models output ports are observable.")
+      end
       super(observer)
     end
 
     # Check if *self* is an input port
     def input?
-      @mode == IO::Input
+      @mode == IOMode::Input
     end
 
     # Check if *self* is an output port
     def output?
-      @mode == IO::Output
+      @mode == IOMode::Output
     end
 
-    def to_s
-      @name.to_s
+    def to_s(io)
+      io << @name
     end
 
-    def inspect
-      "<#{self.class}: name=#{@name}, type=#{@mode}, host=#{@host.name}>"
+    def ==(other : Port)
+      @mode == other.mode && @name == other.name && @host == other.host
+    end
+
+    # :nodoc:
+    def ==(other)
+      false
+    end
+
+    def hash
+      res = 31 * 17 + @mode.hash
+      res = 31 * res + @name.hash
+      31 * res + @host.hash
     end
   end
 end
