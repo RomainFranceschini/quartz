c5ed39700a56e95b15cdcf11b0642f8dd8619510
Romain Franceschini <romain1189@gmail.com>
2016-10-21 10:09:27 +0200

    reorganised hook specs + added count specs

--- spec/hooks_spec.cr
+++ spec/hooks_spec.cr
@@ -18,64 +18,93 @@ private class RaiseNotifiable < MyNotifiable
 end
 
 describe "Hooks" do
-  describe "implementation" do
-
-    describe "subscribe" do
-      it "accepts blocks" do
-        notifier = Hooks::Notifier.new
-        notifier.subscribe(:foo) {}
-      end
-
-      it "accepts notifiable" do
-        notifier = Hooks::Notifier.new
-        notifier.subscribe(:foo, MyNotifiable.new)
-      end
+  describe "#subscribe" do
+    it "accepts blocks" do
+      notifier = Hooks::Notifier.new
+      notifier.subscribe(:foo) {}
     end
 
-    describe "notify" do
-      it "notifies right subscribers" do
-        notifier = Hooks::Notifier.new
-        notifiable = MyNotifiable.new
-        notifier.subscribe(:foo, notifiable)
-        calls = 0
-        block = Proc(Symbol,Nil).new { calls+=1 }
-        notifier.subscribe(:bar, &block)
+    it "accepts notifiable" do
+      notifier = Hooks::Notifier.new
+      notifier.subscribe(:foo, MyNotifiable.new)
+    end
+  end
 
-        notifier.notify(:foo)
-        notifiable.calls.should eq(1)
-        calls.should eq(0)
+  describe "#count_listeners" do
+    it "counts listeners for a given hook" do
+      notifier = Hooks::Notifier.new
+      notifier.subscribe(:foo) {}
+      notifier.subscribe(:foo, MyNotifiable.new)
+      notifier.count_listeners(:foo).should eq(2)
 
-        notifier.notify(:bar)
-        calls.should eq(1)
-        notifiable.calls.should eq(1)
-      end
+      myproc = ->(s : Symbol) {}
+      mynotifiable = MyNotifiable.new
 
-      it "doens't fails when a subscriber raises" do
-        notifier = Hooks::Notifier.new
-        notifier.subscribe(:foo) { raise "ohno" }
-        notifier.subscribe(:foo, RaiseNotifiable.new)
-        notifier.notify(:foo)
-      end
-
-      it "automatically unsubscribes notifiables that raised" do
-        notifier = Hooks::Notifier.new
-        notifiable = RaiseNotifiable.new
-        notifier.subscribe(:foo, notifiable)
-        calls = 0
-        raiser = Proc(Symbol,Nil).new { calls+=1; raise "ohno" }
-        notifier.subscribe(:foo, &raiser)
-
-        notifier.notify(:foo)
-        notifier.notify(:foo)
-
-        notifiable.calls.should eq(1)
-        calls.should eq(1)
-
-        notifier.unsubscribe(:foo, notifiable).should be_false
-        notifier.unsubscribe(:foo, raiser).should be_false
-      end
+      notifier.subscribe(:bar, &myproc)
+      notifier.subscribe(:bar, mynotifiable)
+      notifier.count_listeners(:bar).should eq(2)
+      notifier.unsubscribe(:bar, myproc)
+      notifier.count_listeners(:bar).should eq(1)
+      notifier.unsubscribe(:bar, mynotifiable)
+      notifier.count_listeners(:bar).should eq(0)
     end
 
+    it "counts all listeners" do
+      notifier = Hooks::Notifier.new
+      notifier.subscribe(:foo) {}
+      notifier.subscribe(:bar) {}
+      notifier.count_listeners.should eq(2)
+    end
+  end
+
+  describe "#notify" do
+    it "notifies right subscribers" do
+      notifier = Hooks::Notifier.new
+      notifiable = MyNotifiable.new
+      notifier.subscribe(:foo, notifiable)
+      calls = 0
+      block = Proc(Symbol,Nil).new { calls+=1 }
+      notifier.subscribe(:bar, &block)
+
+      notifier.notify(:foo)
+      notifiable.calls.should eq(1)
+      calls.should eq(0)
+
+      notifier.notify(:bar)
+      calls.should eq(1)
+      notifiable.calls.should eq(1)
+    end
+
+    it "doens't fails when a subscriber raises" do
+      notifier = Hooks::Notifier.new
+      notifier.subscribe(:foo) { raise "ohno" }
+      notifier.subscribe(:foo, RaiseNotifiable.new)
+      notifier.notify(:foo)
+    end
+
+    it "automatically unsubscribes notifiables that raised" do
+      notifier = Hooks::Notifier.new
+
+      i = 0
+      block = ->(s : Symbol) { i += 1; raise "ohno" }
+      notifier.subscribe(:foo, &block)
+
+      notifiable = RaiseNotifiable.new
+      notifier.subscribe(:foo, notifiable)
+
+      notifier.count_listeners(:foo).should eq(2)
+      notifier.notify(:foo)
+      notifier.count_listeners(:foo).should eq(0)
+
+      notifiable.calls.should eq(1)
+      i.should eq(1)
+
+      notifier.unsubscribe(:foo, notifiable).should be_false
+      notifier.unsubscribe(:foo, block).should be_false
+    end
+  end
+
+  describe "#clear" do
     it "clears all subscribers" do
       notifier = Hooks::Notifier.new
       notifiable = MyNotifiable.new
@@ -99,58 +128,60 @@ describe "Hooks" do
       notifier = Hooks::Notifier.new
       notifiable = MyNotifiable.new
       notifier.subscribe(:foo, notifiable)
-      calls = 0
-      block = Proc(Symbol,Nil).new { calls+=1 }
+
+      j = 0
+      block = Proc(Symbol,Nil).new { j+=1 }
       notifier.subscribe(:bar, &block)
 
-      notifier.clear(:bar)
+      notifier.clear(:foo)
 
-      notifier.notify(:foo)
-      notifiable.calls.should eq(1)
       notifier.notify(:bar)
-      calls.should eq(0)
+      notifier.notify(:foo)
 
-      notifier.unsubscribe(:bar, block).should be_false
+      notifiable.calls.should eq(0)
+      j.should eq(1)
+      notifier.unsubscribe(:foo, notifiable).should be_false
+      notifier.unsubscribe(:bar, block).should be_true
+    end
+  end
+
+  describe "#unsubscribe" do
+    it "is falsey when subscriber doesn't exist" do
+      notifier = Hooks::Notifier.new
+      notifiable = MyNotifiable.new
+      notifiable2 = MyNotifiable.new
+      notifier.subscribe(:bar, notifiable)
+
+      notifier.unsubscribe(:foo, notifiable).should be_false
+      notifier.unsubscribe(:bar, notifiable2).should be_false
     end
 
-    describe "unsubscribe" do
-      it "is falsey when subscriber doesn't exist" do
-        notifier = Hooks::Notifier.new
-        notifiable = MyNotifiable.new
-        notifiable2 = MyNotifiable.new
-        notifier.subscribe(:bar, notifiable)
+    it "is truthy when successful" do
+      notifier = Hooks::Notifier.new
+      notifiable = MyNotifiable.new
+      notifiable2 = MyNotifiable.new
+      calls = 0
+      block = Proc(Symbol,Nil).new { calls+=1 }
 
-        notifier.unsubscribe(:foo, notifiable).should be_false
-        notifier.unsubscribe(:bar, notifiable2).should be_false
-      end
+      notifier.subscribe(:foo, notifiable)
+      lost_calls = 0
+      notifier.subscribe(:foo) { lost_calls += 1 }
+      notifier.subscribe(:bar, notifiable2)
+      notifier.subscribe(:bar, &block)
 
-      it "is truthy when successful" do
-        notifier = Hooks::Notifier.new
-        notifiable = MyNotifiable.new
-        notifiable2 = MyNotifiable.new
-        calls = 0
-        block = Proc(Symbol,Nil).new { calls+=1 }
+      notifier.unsubscribe(:foo, notifiable).should be_true
+      notifier.notify(:foo)
+      notifiable.calls.should eq(0)
+      lost_calls.should eq(1)
+      notifier.clear(:foo)
+      notifier.notify(:foo)
+      lost_calls.should eq(1)
 
-        notifier.subscribe(:foo, notifiable)
-        lost_calls = 0
-        notifier.subscribe(:foo) { lost_calls += 1 }
-        notifier.subscribe(:bar, notifiable2)
-        notifier.subscribe(:bar, &block)
-
-        notifier.unsubscribe(:foo, notifiable).should be_true
-        notifier.notify(:foo)
-        notifiable.calls.should eq(0)
-        lost_calls.should eq(1)
-        notifier.clear(:foo)
-        notifier.notify(:foo)
-        lost_calls.should eq(1)
-
-        notifier.unsubscribe(:bar, notifiable2).should be_true
-        notifier.unsubscribe(:bar, block).should be_true
-        notifier.notify(:bar)
-        notifiable2.calls.should eq(0)
-        calls.should eq(0)
-      end
+      notifier.unsubscribe(:bar, notifiable2).should be_true
+      notifier.unsubscribe(:bar, block).should be_true
+      notifier.notify(:bar)
+      notifiable2.calls.should eq(0)
+      calls.should eq(0)
     end
   end
 end
