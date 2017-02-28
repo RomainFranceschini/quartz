RomainFranceschini/quartz

commit d4e0da7f5937062be1f8c1d90066b85ce06c9bb4

validations wip 3

src/quartz/simulation.cr
@@ -12,8 +12,14 @@ module Quartz
     @processor : Coordinator
     @start_time : Time?
     @final_time : Time?
+    @run_validations : Bool
 
-    def initialize(model : Model, *, scheduler : Symbol = :calendar_queue, maintain_hierarchy : Bool = true, duration : SimulationTime = Quartz::INFINITY)
+    def initialize(
+                   model : Model, *,
+                   scheduler : Symbol = :calendar_queue,
+                   maintain_hierarchy : Bool = true,
+                   duration : SimulationTime = Quartz::INFINITY,
+                   run_validations : Bool = false)
       @time = 0
 
       @model = case model
@@ -25,6 +31,7 @@ module Quartz
 
       @duration = duration
       @scheduler = scheduler
+      @run_validations = run_validations
 
       # TODO check direct_connect correctness
       unless maintain_hierarchy
@@ -45,6 +52,16 @@ module Quartz
       io << ", time=" << @time.to_s(io)
       io << ", duration=" << @duration.to_s(io)
       nil
+    end
+
+    # Returns the default scheduler to use.
+    def default_scheduler
+      @scheduler
+    end
+
+    # Whether `Quartz::Validations` will be run during simulation.
+    def run_validations?
+      @run_validations
     end
 
     # Returns *true* if the simulation is done, *false* otherwise.
@@ -300,13 +317,13 @@ module Quartz
 
     def allocate_processors(coupled = @model)
       is_root = coupled == @model
-      processor = ProcessorFactory.processor_for(coupled, @scheduler, is_root).as(Coordinator)
+      processor = ProcessorFactory.processor_for(coupled, self, is_root).as(Coordinator)
 
       coupled.as(CoupledModel).each_child do |model|
         processor << if model.is_a?(CoupledModel)
           allocate_processors(model)
         else
-          ProcessorFactory.processor_for(model, @scheduler)
+          ProcessorFactory.processor_for(model, self)
         end
       end
 

src/quartz/validations.cr
@@ -4,14 +4,36 @@ require "./validators/numericality"
 require "./validators/inclusion"
 
 module Quartz
+
+  # Provides a validation framework for your models.
+  #
+  # Example:
+  # ```
+  # class WeightModel
+  #   include Quartz::Validations
+  #
+  #   property weight : Float64 = 0.0  # in kg
+  #   validates :weight, numericality: { greater_than: 40, lesser_than: 160 }
+  # end
+  #
+  # model = WeightModel.new
+  # model.weight = 75.0
+  # model.valid?              # => true
+  # model.invalid?            # => false
+  #
+  # model.weight = 200.0
+  # model.valid?              # => false
+  # model.invalid?            # => true
+  # model.errors.messages     # => { :weight => ["must be lesser than 160"] }
+  # ```
   module Validations
     macro included
       def self.validators
-        @@validators ||= {} of Symbol => Array(Validators::AbstractValidator)
+        @@validators ||= Array(Validators::Validator).new
       end
 
       def self.clear_validators
-        @@validators.clear
+        @@validators.try &.clear
       end
 
       def self.validates(*attributes : Symbol, **kwargs)
@@ -19,18 +41,89 @@ module Quartz
           raise ArgumentError.new("You must inform at least one validation rule")
         end
 
+        kwargs.each do |name, value|
+          validator = case name
+          when :presence
+            if value.is_a?(NamedTuple)
+              Validators::PresenceValidator.new(*attributes, **value)
+            else
+              Validators::PresenceValidator.new(*attributes)
+            end
+          when :numericality
+            if value.is_a?(NamedTuple)
+              Validators::NumericalityValidator.new(*attributes, **value)
+            else
+              Validators::NumericalityValidator.new(*attributes)
+            end
+          else
+            raise ArgumentError.new("Unknown validator \"#{name}\"")
+          end
 
+          validators.push(validator)
+        end
+      end
+
+      # Passes the model off to the class or classes specified and allows them
+      # to add errors based on more complex conditions.
+      #
+      # ```
+      # class MyModel
+      #   include Quartz::Validations
+      #   validates_with MyValidator
+      # end
+      #
+      # class MyValidator < Quartz::Validators::EachValidator
+      #   def validate_each(model, attribute, value)
+      #     if some_test
+      #       model.errors.add(attribute, "This model attribute is invalid")
+      #     end
+      #   end
+      #
+      #   # ...
+      # end
+      # ```
+      def self.validates_with(klass : Validators::EachValidator.class, *attributes : Symbol, **kwargs)
+        validator = klass.new(*attributes, **kwargs)
+        validators.push(validator)
+      end
+
+      # Passes the model off to the class or classes specified and allows them
+      # to add errors based on more complex conditions.
+      #
+      # ```
+      # class MyModel
+      #   include Quartz::Validations
+      #   validates_with MyValidator
+      # end
+      #
+      # class MyValidator < Quartz::Validators::Validator
+      #   def validate(model)
+      #     if some_test
+      #       model.errors.add(:phase, "This model state is invalid")
+      #     end
+      #   end
+      #
+      #   # ...
+      # end
+      # ```
+      def self.validates_with(klass : Validators::Validator.class, **kwargs)
+        validator = klass.new(**kwargs)
+        validators.push(validator)
       end
 
       # TODO: Copy validators on inheritance
       macro inherited
-
       end
     end
 
-    # Returns the `Errors` object that holds all information about attribute
-    # error messages.
-    getter(errors) { Quartz::Errors.new }
+    # Returns the `ValidationErrors` object that holds all information about
+    # attribute error messages.
+    getter(errors) { Quartz::ValidationErrors.new }
+
+    # Clears attribute error messages.
+    def clear_errors
+      @errors.try &.clear
+    end
 
     def attributes
       values = {{ ("{} of Symbol => " + @type.instance_vars.map(&.type).join("|")).id }}
@@ -42,14 +135,49 @@ module Quartz
       values
     end
 
-    def valid? : Bool
-
+    # Runs all the specified validations and returns *true* if no errors were
+    # added otherwise *false*.
+    #
+    def valid?(context : Symbol? = nil) : Bool
+      errors.clear
+      run_validators(context)
+      errors.empty?
     end
 
-    def valid?(context : Symbol) : Bool
-
+    # Performs the opposite of `#valid?`. Returns *true* if errors were added,
+    # *false* otherwise.
+    #
+    # Usage:
+    # ```
+    # class MyModel
+    #   include Quartz::Validations
+    #
+    #   property :phase : String?
+    #   validates :phase, presence: true
+    # end
+    #
+    # model = MyModel.new
+    # model.phase = ""
+    # model.invalid?          # => true
+    # model.phase = "idle"
+    # model.invalid?          # => false
+    # ```
+    #
+    # Context can optionally be supplied to define which validators to test
+    # against (the context is defined on the validators using *on:* option).
+    def invalid?(context : Symbol? = nil) : Bool
+      !valid?(context)
     end
 
-
+    protected def run_validators(context : Symbol? = nil)
+      self.class.validators.each do |validator|
+        contexts = validator.contexts
+        if !contexts || contexts.includes?(context)
+          if !validator.validate(self) && validator.strict?
+            raise StrictValidationFailed.new(errors)
+          end
+        end
+      end
+    end
   end
 end

spec/errors_spec.cr
@@ -1,10 +1,10 @@
 require "./spec_helper"
 
-describe "Errors" do
+describe "ValidationErrors" do
   describe "#add" do
     context "with string" do
       it "append error to the attribute list of errors" do
-        errors = Quartz::Errors.new
+        errors = Quartz::ValidationErrors.new
         errors.add(:name, "My first error")
         errors.add(:age, "My second error")
         errors.add(:age, "My third error")
@@ -17,7 +17,7 @@ describe "Errors" do
 
     context "with array of strings" do
       it "append multiple errors to the attribute list of errors" do
-        errors = Quartz::Errors.new
+        errors = Quartz::ValidationErrors.new
         errors.add(:name, "My first error")
         errors.add(:age, "My second error", "My third error")
         errors.messages.should eq({
@@ -26,20 +26,11 @@ describe "Errors" do
         })
       end
     end
-
-    context "with strict option enabled" do
-      it "raises when appending errors" do
-        errors = Quartz::Errors.new
-        expect_raises(StrictValidationFailed, "my_attr is invalid") do
-          errors.add(:my_attr, "is invalid", strict: true)
-        end
-      end
-    end
   end
 
   describe "#clear" do
     it "clear list of errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors.add(:age, "My second error", "My third error")
       errors.clear
       errors.messages.should eq({} of Symbol => Array(String))
@@ -48,7 +39,7 @@ describe "Errors" do
 
   describe "#size" do
     it "returns 0 when there are no errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors.size.should eq(0)
 
       errors.add(:name, "My first error")
@@ -57,7 +48,7 @@ describe "Errors" do
     end
 
     it "counts all errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors.add(:name, "My first error")
       errors.add(:age, "My second error", "My third error")
       errors.size.should eq(3)
@@ -66,13 +57,13 @@ describe "Errors" do
 
   describe "#include?" do
     it "is truthy when given attribute has errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors.add(:name, "My first error")
       errors.include?(:name).should be_true
     end
 
     it "is falsey when given attribute has no errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors.include?(:test).should be_false
 
       errors.add(:name, "My first error")
@@ -83,13 +74,13 @@ describe "Errors" do
 
   describe "#[]" do
     it "returns list of errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors.add(:name, "My first error")
       errors[:name].should eq(["My first error"])
     end
 
     it "returns nil when there are no errors" do
-      errors = Quartz::Errors.new
+      errors = Quartz::ValidationErrors.new
       errors[:name].should be_nil
 
       errors.add(:name, "My first error")

spec/validations_helper.cr
@@ -31,3 +31,10 @@ class NumericModel
                  @nilint = nil)
   end
 end
+
+class SmallModel
+  include Quartz::Validations
+
+  property weight : Float64 = 0.0 # in kg
+  property height : Int32 = 0     # in cm
+end

spec/validators/numericality_spec.cr
@@ -82,5 +82,62 @@ describe "NumericalityValidator" do
         model.errors[:int].should eq(["must be lesser than or equal to 50"])
       end
     end
+
+    context "equal to" do
+      it "adds error for numbers greater than target" do
+        model = NumericModel.new(int: 65)
+        Validators::NumericalityValidator.new(:int, equal_to: 50).validate(model)
+        model.errors[:int].should eq(["must be equal to 50"])
+      end
+
+      it "adds error for numbers lesser than target" do
+        model = NumericModel.new(int: 9)
+        Validators::NumericalityValidator.new(:int, equal_to: 50).validate(model)
+        model.errors[:int].should eq(["must be equal to 50"])
+      end
+
+      it "don't adds errors for numbers equal to target" do
+        model = NumericModel.new(int: 50)
+        Validators::NumericalityValidator.new(:int, equal_to: 50).validate(model)
+        model.errors[:int].should be_nil
+      end
+    end
+
+    context "not equal to" do
+      it "don't adds error for numbers greater than target" do
+        model = NumericModel.new(int: 65)
+        Validators::NumericalityValidator.new(:int, not_equal_to: 50).validate(model)
+        model.errors[:int].should be_nil
+      end
+
+      it "don't adds error for numbers lesser than target" do
+        model = NumericModel.new(int: 9)
+        Validators::NumericalityValidator.new(:int, not_equal_to: 50).validate(model)
+        model.errors[:int].should be_nil
+      end
+
+      it "adds errors for numbers equal to target" do
+        model = NumericModel.new(int: 50)
+        Validators::NumericalityValidator.new(:int, not_equal_to: 50).validate(model)
+        model.errors[:int].should eq(["must be other than 50"])
+      end
+    end
+
+    context "allow nil" do
+      it "don't adds error if number is nan?" do
+        model = NumericModel.new(float: Float64::NAN)
+        Validators::NumericalityValidator.new(:float, allow_nil: true).validate(model)
+        model.errors[:float].should be_nil
+      end
+    end
+
+    context "don't allow nil" do
+      it "don't adds error if number is nan?" do
+        model = NumericModel.new(float: Float64::NAN)
+        Validators::NumericalityValidator.new(:float, allow_nil: false).validate(model)
+        model.errors[:float].should eq(["is not a number"])
+      end
+    end
+
   end
 end

src/quartz/atomic.cr
@@ -4,6 +4,7 @@ module Quartz
     include Coupleable
     include Transitions
     include Observable
+    include Validations
 
     def initialize(name)
       super(name)

src/quartz/coordinator.cr
@@ -7,12 +7,12 @@ module Quartz
     @scheduler : EventSet(Processor)
 
     # Returns a new instance of Coordinator
-    def initialize(model : Model, scheduler : Symbol)
+    def initialize(model : Model, simulation : Simulation)
       super(model)
 
       @children = Array(Processor).new
-      @scheduler_type = model.class.preferred_event_set? || scheduler
-      @scheduler = EventSetFactory(Processor).new_event_set(@scheduler_type)
+      scheduler_type = model.class.preferred_event_set? || simulation.default_scheduler
+      @scheduler = EventSetFactory(Processor).new_event_set(scheduler_type)
       @synchronize = Array(SyncEntry).new
       @parent_bag = Hash(Port,Array(Any)).new { |h,k|
         h[k] = Array(Any).new

src/quartz/dsde/coordinator.cr
@@ -2,6 +2,13 @@ module Quartz
   module DSDE
     class Coordinator < Quartz::Coordinator
 
+      @simulation : Simulation
+
+      def initialize(model, simulation)
+        super(model, simulation)
+        @simulation = simulation
+      end
+
       def perform_transitions(time, bag)
         coupled = @model.as(Quartz::DSDE::CoupledModel)
 
@@ -122,7 +129,7 @@ module Quartz
         # initialize new models and their processors
         new_children = current_children - old_children
         new_children.each do |new_model|
-          processor = ProcessorFactory.processor_for(new_model, @scheduler_type)
+          processor = ProcessorFactory.processor_for(new_model, @simulation)
           self << processor
 
           tn = processor.initialize_processor(time)

commit d4cbe94e47d596a878d4e6231d5f7f080f94c1ca

fix regression caused by d4e0da7f

src/quartz/simulation.cr
@@ -9,13 +9,12 @@ module Quartz
 
     @time : SimulationTime
     @scheduler : Symbol
-    @processor : Coordinator
+    @processor : RootCoordinator?
     @start_time : Time?
     @final_time : Time?
     @run_validations : Bool
 
-    def initialize(
-                   model : Model, *,
+    def initialize(model : Model, *,
                    scheduler : Symbol = :calendar_queue,
                    maintain_hierarchy : Bool = true,
                    duration : SimulationTime = Quartz::INFINITY,
@@ -43,8 +42,13 @@ module Quartz
       end
 
       time = Time.now
-      @processor = allocate_processors
+      self.processor # allocate processors
       info "Allocated processors in #{Time.now - time} secs"
+    end
+
+    @[AlwaysInline]
+    protected def processor
+      @processor ||= self.allocate_processors.as(RootCoordinator)
     end
 
     def inspect(io)
@@ -120,7 +124,7 @@ module Quartz
     # Returns the number of transitions per model along with the total
     def transition_stats
       stats = {} of Name => Hash(Symbol, UInt32)
-      hierarchy = @processor.children.dup
+      hierarchy = self.processor.children.dup
       hierarchy.each do |child|
         if child.is_a?(Coordinator)
           coordinator = child.as(Coordinator)
@@ -183,12 +187,12 @@ module Quartz
 
     private def initialize_simulation
       Hooks.notifier.notify(:before_simulation_initialization_hook)
-      @time = @processor.as(Simulable).initialize_state(@time)
+      @time = self.processor.initialize_state(@time)
       Hooks.notifier.notify(:after_simulation_initialization_hook)
     end
 
     def step : SimulationTime?
-      simulable = @processor.as(Simulable)
+      simulable = self.processor
       if waiting?
         initialize_simulation
         begin_simulation
@@ -208,7 +212,7 @@ module Quartz
     # TODO error hook
     def simulate
       if waiting?
-        simulable = @processor.as(Simulable)
+        simulable = self.processor
         initialize_simulation
         begin_simulation
         while @time < @duration
@@ -234,7 +238,7 @@ module Quartz
 
     def each
       if waiting?
-        simulable = @processor.as(Simulable)
+        simulable = self.processor
         initialize_simulation
         begin_simulation
         while @time < @duration

src/quartz/validations.cr
@@ -1,7 +1,6 @@
 require "./validators/validator"
 require "./validators/presence"
 require "./validators/numericality"
-require "./validators/inclusion"
 
 module Quartz
 

src/quartz/validators/inclusion.cr
@@ -1,36 +1,0 @@
-module HasIncludes
-  abstract def includes?(value)
-end
-
-class String
-  include HasIncludes
-end
-
-struct Range(B, E)
-  include HasIncludes
-end
-
-# module Enumerable(T)
-#   include HasIncludes
-# end
-
-module Quartz
-  module Validators
-    class InclusionValidator < EachValidator
-      getter delimiter : HasIncludes
-
-      def initialize(*attributes, **kwargs)
-        super(*attributes, **kwargs)
-        if (delimiter = (kwargs[:in]? || kwargs[:within])) && delimiter.responds_to?(:includes?) && delimiter.is_a?(HasIncludes)
-          @delimiter = delimiter
-        end
-      end
-
-      def validate_each(model, attribute, value)
-        unless @delimiter.includes?(value)
-          model.errors.add(attribute, "must be included in #{@delimiter}")
-        end
-      end
-    end
-  end
-end