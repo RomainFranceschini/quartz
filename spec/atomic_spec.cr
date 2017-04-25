RomainFranceschini/quartz

commit bd11d1d68b74e738862ad463e33bd322f7cd7416

specs and fixes related to `state_var` macro, update examples to use state_var

src/quartz/atomic.cr
@@ -12,21 +12,20 @@ module Quartz
       @bag = SimpleHash(OutputPort, Any).new
     end
 
-    macro inherited
-      def initialize(name)
-        super(name)
-      end
-
-      def initialize(name, state : {{(@type.name + "::State").id}})
-        super(name)
-        self.state = state
-      end
+    def initialize(name, state)
+      super(name)
+      @bag = SimpleHash(OutputPort, Any).new
+      self.state = state
     end
 
     # :nodoc:
     # Used internally by the simulator
     def __initialize_state__(processor)
-      if processor == @processor && (state = initial_state)
+      if @processor != processor
+        raise InvalidProcessorError.new("trying to initialize state of model \"#{name}\" from an invalid processor")
+      end
+
+      if state = initial_state
         self.state = state
       end
     end

src/quartz/multi_component/component.cr
@@ -1,10 +1,5 @@
 module Quartz
   module MultiComponent
-    abstract struct ComponentState
-      # Cause bugs, transferable has to be included directly in final struct state
-      # include Transferable
-    end
-
     abstract class Component < Model
       include Transitions
       include Observable
@@ -17,21 +12,23 @@ module Quartz
       getter influencers = Array(Component).new
       getter influencees = Array(Component).new
 
-      macro inherited
-        def initialize(name)
-          super(name)
-        end
-
-        def initialize(name, state : {{(@type.name + "::State").id}})
-          super(name)
-          self.state = state
-        end
+      def initialize(name)
+        super(name)
       end
 
-      # :nodoc:
+      def initialize(name, state)
+        super(name)
+        self.state = state
+      end
+
       # Used internally by the simulator
+      # :nodoc:
       def __initialize_state__(processor)
-        if processor == @processor && (state = initial_state)
+        if @processor != processor
+          raise InvalidProcessorError.new("trying to initialize state of model \"#{name}\" from an invalid processor")
+        end
+
+        if state = initial_state
           self.state = state
         end
       end

src/quartz/state.cr
@@ -8,7 +8,7 @@ module Quartz
     macro included
       {% if !@type.constant :STATE_VARIABLES %}
         private STATE_VARIABLES = [] of Nil
-        private AFTER_INITIALIZE = [] of Nil
+        private STATE_INITIALIZE = [] of Nil
       {% end %}
 
       # The `state_var` macro defines a state variable of a model. Its primary
@@ -41,35 +41,86 @@ module Quartz
       #   state_var x : Int32 = 0
       #   state_var y : Int32 = 0
       #   state_var z : Int32 { (rand * 100 + 1).to_i32 }
+      #   state_var ß : Int32 { x * 42 }
       # end
       # ```
       #
+      # Note from previous example that the initialization block of ß is
+      # allowed to reference the value of another state variable.
+      #
       # If default values are omitted, a chance is given to initialize those
-      # state variables through a constructor or using the `after_initialize`
-      # macro :
+      # state variables through the `state_initialize` macro :
       #
       # ```
       # class MyModel < AtomicModel
-      #   state_var x : Int32 = 0
-      #   state_var y : Int32 = 0
+      #   state_var x : Int32
+      #   state_var y : Int32
       #
-      #   def initialize
+      #   state_initialize do
       #     @x = 0
       #     @y = 0
       #   end
       # end
       # ```
       #
-      # `state_var` also accept a hash or named tuple literal whose
-      # keys corresponds to the following options :
-      # * *visibility*: used to restrict the visibility of the getter that
-      # is defined for this state variable (`:private` or `:protected`). No
-      # restriction is applied by default (public).
-      # * *converter*: specify an alternate type for parsing and generation.
-      # The converter must define `from_json(JSON::PullParser)` and
-      # `to_json(value, JSON::Builder)` as class methods. Examples of converters
-      # are `Time::Format` and `Time::EpochConverter` for `Time`.
+      # All `initialize` methods defined in the included type and its subclasses
+      # will be redefined to include the body of the given block.
+      # Note that the block content is always included at the top of the method
+      # definition. Thus, if you define :
       #
+      # ```
+      # class MyModel < AtomicModel
+      #   state_var x : Int32
+      #   state_var y : Int32
+      #
+      #   state_initialize do
+      #     @x = 0
+      #     @y = 0
+      #   end
+      #
+      #   def initialize(name)
+      #     super(name)
+      #     add_input_port("in")
+      #   end
+      # end
+      # ```
+      #
+      # The constructor will be automatically redefined to :
+      #
+      # ```
+      #   def initialize(name)
+      #     @x = 0
+      #     @y = 0
+      #     super(name)
+      #     add_input_port("in")
+      #   end
+      # end
+      # ```
+      #
+      # #### Options
+      #
+      # Along with the type declaration, `state_var` also accept a hash or
+      # named tuple literal whose keys corresponds to the following options :
+      # * **visibility**: used to restrict the visibility of the getter that
+      # is defined for this state variable (`:private` or `:protected`). No
+      # restriction is applied by default (public) :
+      # ```
+      # state_var hidden : Bool = true, visibility: :private
+      # ```
+      # * **converter**: specify an alternate type for parsing and generation.
+      # Examples of converters are `Time::Format` and `Time::EpochConverter`
+      # for `Time`.
+      # ```
+      # state_var timestamp : Time, converter: Time::EpochConverter
+      # ```
+      #
+      # #### Code generation
+      #
+      # A getter is generated for each declared state variable, whose visibility
+      # can be restricted through the **visibility** option.
+      #
+      # When the type definition is complete, a struct wrapping all state
+      # variables is defined for convenience.
       macro state_var(prop, **opts, &block)
         \{%
           opts[:name] = prop.var
@@ -92,8 +143,61 @@ module Quartz
         %}
       end
 
-      macro after_initialize(&block)
-        \{% AFTER_INITIALIZE << block.body %}
+      # The `state_initialize` macro defines an initialization block
+      # that is automatically included in all constructor defined in the
+      # included type and its subclasses.
+      #
+      # It can be used to initialize state variables declared using the
+      # `state_var` macro.
+      #
+      # Example :
+      #
+      # ```
+      # class MyModel < AtomicModel
+      #   state_var x : Int32
+      #   state_var y : Int32
+      #
+      #   state_initialize do
+      #     @x = 0
+      #     @y = 0
+      #   end
+      # ```
+      #
+      macro state_initialize(&block)
+        \{% STATE_INITIALIZE << block.body %}
+      end
+
+      private macro redefine_constructors
+        \{% if @type.methods.any? { |m| m.name.stringify == "initialize" } %}
+          \{% for method in @type.methods %}
+            \{% if method.name == "initialize" %}
+              \{{ method.visibility.id }} def \{{ method.name.id }}(
+                \{% if method.splat_index.is_a?(NumberLiteral) %}
+                  \{% for arg, index in method.args %}
+                    \{{ (index == method.splat_index ? "*" + arg.stringify : arg.stringify).id }},
+                  \{% end %}
+                \{% else %}
+                  \{{ method.args.splat }} \{{ (method.double_splat.is_a?(Nop) && method.block_arg.is_a?(Nop) ? "" : ",").id }}
+                \{% end %}
+                \{{ (method.double_splat.is_a?(Nop) ? "" : "**" + method.double_splat.stringify).id }}
+                \{{ (method.block_arg.is_a?(Nop) ? "" : "&" + method.block_arg.stringify).id }}
+              ) \{{ (method.return_type.is_a?(Nop) ? "" : ":").id }} \{{ method.return_type }}
+
+                \{% for block in STATE_INITIALIZE %}
+                  \{{ block }}
+                \{% end %}
+
+                \{{ method.body }}
+              end
+            \{% end %}
+          \{% end %}
+        \{% else %}
+          def initialize
+            \{% for block in STATE_INITIALIZE %}
+              \{{ block }}
+            \{% end %}
+          end
+        \{% end %}
       end
 
       macro finished
@@ -119,10 +223,15 @@ module Quartz
           \{% end %}
         \{% end %}
 
-        \{% if !STATE_VARIABLES.empty? %}
+        \{% if !STATE_INITIALIZE.empty? %}
+          redefine_constructors
+        \{% end %}
 
+        \{% if !STATE_VARIABLES.empty? %}
           # A struct that wraps the state of a model
           struct \{{ @type.name.id }}::State < Quartz::State
+            include Quartz::Transferable
+
             \{% for var in STATE_VARIABLES %}
               getter \{{var[:name].id}} : \{{var[:type]}}
             \{% end %}
@@ -140,6 +249,10 @@ module Quartz
               \{% end %}
               **args
             )
+              \{% for block in STATE_INITIALIZE %}
+                \{{ block }}
+              \{% end %}
+
               \{% for var in STATE_VARIABLES %}
                 \{% if var[:value] != nil %}
                   \{% if var[:value].is_a?(Block) %}
@@ -160,9 +273,36 @@ module Quartz
                   end
                 \{% end %}
               \{% end %}
+            end
 
-              \{% for block in AFTER_INITIALIZE %}
+            def initialize(
+              \{% for var in STATE_VARIABLES %}
+                \{% if var[:value] != nil %}
+                  \{% if var[:value].is_a?(Block) %}
+                    \{{ var[:name].id }} : \{{ var[:type] }}? = nil,
+                  \{% elsif var[:name] == nil %}
+                    \{{ var[:name].id }} : \{{ var[:type] }},
+                  \{% else %}
+                    \{{ var[:name].id }} : \{{ var[:type] }} = \{{ var[:value] }},
+                  \{% end %}
+                \{% end %}
+              \{% end %}
+            )
+              \{% for block in STATE_INITIALIZE %}
                 \{{ block }}
+              \{% end %}
+
+              \{% for var in STATE_VARIABLES %}
+                \{% if var[:value] != nil %}
+                  \{% if var[:value].is_a?(Block) %}
+                    if \{{ var[:name].id }}.nil?
+                      \{{ var[:name].id }} = (
+                        \{{ var[:value].body }}
+                      )
+                    end
+                  \{% end %}
+                  @\{{ var[:name].id }} = \{{ var[:name].id }}
+                \{% end %}
               \{% end %}
             end
 
@@ -171,6 +311,10 @@ module Quartz
                 \{{ var[:type] }} \{{ (index == STATE_VARIABLES.size-1 ? ")" : "|").id }}
               \{% end %}
             ))
+              \{% for block in STATE_INITIALIZE %}
+                \{{ block }}
+              \{% end %}
+
               \{% for var in STATE_VARIABLES %}
                 \{% if var[:value] != nil %}
                   if !hash.has_key?(:\{{ var[:name].id }})
@@ -193,10 +337,6 @@ module Quartz
                   end
                 \{% end %}
               \{% end %}
-
-              \{% for block in AFTER_INITIALIZE %}
-                \{{ block }}
-              \{% end %}
             end
 
             def initialize(hash : Hash(String, Union(
@@ -204,6 +344,10 @@ module Quartz
                 \{{ var[:type] }} \{{ (index == STATE_VARIABLES.size-1 ? ")" : "|").id }}
               \{% end %}
             ))
+              \{% for block in STATE_INITIALIZE %}
+                \{{ block }}
+              \{% end %}
+
               \{% for var in STATE_VARIABLES %}
                 \{% if var[:value] != nil %}
                   if !hash.has_key?(\{{ var[:name].stringify }})
@@ -226,13 +370,13 @@ module Quartz
                   end
                 \{% end %}
               \{% end %}
-
-              \{% for block in AFTER_INITIALIZE %}
-                \{{ block }}
-              \{% end %}
             end
 
             def initialize(%pull : ::JSON::PullParser)
+              \{% for block in STATE_INITIALIZE %}
+                \{{ block }}
+              \{% end %}
+
               \{% for var in STATE_VARIABLES %}
                 \%found{var[:name].id} = false
                 \%json{var[:name].id} = nil
@@ -265,17 +409,21 @@ module Quartz
                   else
                     \{{ var[:value].body }}
                   end
+                \{% elsif var[:value] == nil %}
+                  if \%found{var[:name].id} && (value = \%json{var[:name].id})
+                    @\{{var[:name].id}} = value.as(\{{var[:type]}})
+                  end
                 \{% else %}
                   @\{{var[:name].id}} = \%found{var[:name].id} ? \%json{var[:name].id}.as(\{{var[:type]}}) : \{{var[:value]}}
                 \{% end %}
               \{% end %}
-
-              \{% for block in AFTER_INITIALIZE %}
-                \{{ block }}
-              \{% end %}
             end
 
             def initialize(%pull : ::MessagePack::Unpacker)
+              \{% for block in STATE_INITIALIZE %}
+                \{{ block }}
+              \{% end %}
+
               \{% for var in STATE_VARIABLES %}
                 \%found{var[:name].id} = false
                 \%mp{var[:name].id} = nil
@@ -308,13 +456,13 @@ module Quartz
                   else
                     \{{ var[:value].body }}
                   end
+                \{% elsif var[:value] == nil %}
+                  if \%found{var[:name].id} && (value = \%mp{var[:name].id})
+                    @\{{var[:name].id}} = value.as(\{{var[:type]}})
+                  end
                 \{% else %}
                   @\{{var[:name].id}} = \%found{var[:name].id} ? \%mp{var[:name].id}.as(\{{var[:type]}}) : \{{var[:value]}}
                 \{% end %}
-              \{% end %}
-
-              \{% for block in AFTER_INITIALIZE %}
-                \{{ block }}
               \{% end %}
             end
 
@@ -394,15 +542,18 @@ module Quartz
             end
           end
 
-          # Function used
           def initial_state=(state : \{{@type.name.id}}::State)
             @_initial_state = state
           end
 
-          @_initial_state : \{{@type.name.id}}::State?
+          @_initial_state : Quartz::State?
 
           protected def initial_state
-            @_initial_state
+            if state = @_initial_state
+              state.as(\{{@type.name.id}}::State)
+            else
+              nil
+            end
           end
 
           def state
@@ -418,17 +569,14 @@ module Quartz
               @\{{ var[:name].id }} = state.\{{ var[:name].id }}
             \{% end %}
           end
-
         \{% else %}
           def state
-            Quartz::State.new
           end
 
           protected def state=(state : Quartz::State)
           end
 
           protected def initial_state
-            nil
           end
         \{% end %}
       end

spec/atomic_spec.cr
@@ -14,8 +14,25 @@ private class FetchOutputTest < AtomicModel
   end
 end
 
+private class ModelSample < AtomicModel
+  state_var x : Int32 = 0
+  state_var y : Int32 = 0
+
+  @sigma = 25
+end
+
+private class MockProcessor < Processor
+  def collect_outputs(time)
+    Hash(OutputPort, Any).new
+  end
+
+  def perform_transitions(time, bag : Hash(InputPort, Array(Any)))
+    0
+  end
+end
+
 describe "AtomicModel" do
-  describe "post" do
+  describe "#post" do
     it "raises when dropping a value on a port of another model" do
       foo = AtomicModel.new("foo")
       bar = AtomicModel.new("bar")
@@ -34,11 +51,100 @@ describe "AtomicModel" do
     end
   end
 
+  describe "#initialize" do
+    it "can be initialized using its state" do
+      m = ModelSample.new("foo", ModelSample::State.new(x: 1, y: 2))
+      m.x.should eq 1
+      m.y.should eq 2
+    end
+  end
+
+  describe "simulation parameters initialization" do
+    it "uses initial state to set its state before running a simulation" do
+      m = ModelSample.new("foo")
+      m.x.should eq 0
+      m.y.should eq 0
+
+      m.initial_state = ModelSample::State.new(x: 5, y: 10)
+      m.x.should eq 0
+      m.y.should eq 0
+
+      p = MockProcessor.new(m)
+      m.__initialize_state__(p)
+
+      m.x.should eq 5
+      m.y.should eq 10
+    end
+
+    it "raises if wrong processor asks initialization" do
+      m = ModelSample.new("foo")
+      p1 = MockProcessor.new(m)
+      p2 = MockProcessor.new(m)
+      expect_raises InvalidProcessorError do
+        m.__initialize_state__(p1)
+      end
+    end
+  end
+
   describe "fetch_output!" do
     it "calls #output" do
       m = FetchOutputTest.new
       m.fetch_output![m.output_port("out")].should eq("a value")
       m.calls.should eq(1)
+    end
+  end
+
+  describe "serialization" do
+    it "can be converted to JSON" do
+      m = ModelSample.new("foo", ModelSample::State.new(x: 5, y: 10))
+      m.to_json.should eq "{\"name\":\"foo\",\"state\":{\"x\":5,\"y\":10},\"sigma\":25}"
+      m.time = 42
+      m.to_json.should eq "{\"name\":\"foo\",\"state\":{\"x\":5,\"y\":10},\"time\":42,\"sigma\":25}"
+    end
+
+    it "can be converted to msgpack" do
+      m = ModelSample.new("foo", ModelSample::State.new(x: 5, y: 10))
+      m.to_msgpack.should eq Bytes[132, 164, 110, 97, 109, 101, 163, 102, 111, 111, 165, 115, 116, 97, 116, 101, 130, 161, 120, 5, 161, 121, 10, 164, 116, 105, 109, 101, 202, 255, 128, 0, 0, 165, 115, 105, 103, 109, 97, 25]
+      m.time = 42
+      m.to_msgpack.should eq Bytes[132, 164, 110, 97, 109, 101, 163, 102, 111, 111, 165, 115, 116, 97, 116, 101, 130, 161, 120, 5, 161, 121, 10, 164, 116, 105, 109, 101, 42, 165, 115, 105, 103, 109, 97, 25]
+    end
+  end
+
+  describe "deserialization" do
+    it "can be initialized from JSON" do
+      io = IO::Memory.new("{\"name\":\"foo\",\"state\":{\"x\":5,\"y\":10},\"sigma\":0}")
+      m = ModelSample.new(JSON::PullParser.new(io))
+      m.name.should eq("foo")
+      m.sigma.should eq 0
+      m.time.should eq -INFINITY
+      m.x.should eq 5
+      m.y.should eq 10
+
+      io = IO::Memory.new("{\"name\":\"foo\",\"state\":{\"x\":5,\"y\":10},\"sigma\":0,\"time\":100}")
+      m = ModelSample.new(JSON::PullParser.new(io))
+      m.name.should eq("foo")
+      m.sigma.should eq 0
+      m.time.should eq 100
+      m.x.should eq 5
+      m.y.should eq 10
+    end
+
+    it "can be initialized from msgpack" do
+      io = IO::Memory.new(Bytes[132, 164, 110, 97, 109, 101, 163, 102, 111, 111, 165, 115, 116, 97, 116, 101, 130, 161, 120, 5, 161, 121, 10, 164, 116, 105, 109, 101, 202, 255, 128, 0, 0, 165, 115, 105, 103, 109, 97, 25])
+      m = ModelSample.new(MessagePack::Unpacker.new(io))
+      m.name.should eq "foo"
+      m.sigma.should eq 25
+      m.time.should eq -INFINITY
+      m.x.should eq 5
+      m.y.should eq 10
+
+      io = IO::Memory.new(Bytes[132, 164, 110, 97, 109, 101, 163, 102, 111, 111, 165, 115, 116, 97, 116, 101, 130, 161, 120, 5, 161, 121, 10, 164, 116, 105, 109, 101, 42, 165, 115, 105, 103, 109, 97, 25])
+      m = ModelSample.new(MessagePack::Unpacker.new(io))
+      m.name.should eq "foo"
+      m.sigma.should eq 25
+      m.time.should eq 100
+      m.x.should eq 5
+      m.y.should eq 10
     end
   end
 

examples/dyn_traffic_light.cr
@@ -1,14 +1,11 @@
 require "../src/quartz"
 
 class TrafficLight < Quartz::AtomicModel
-  getter phase : Symbol
 
-  def initialize(name)
-    super(name)
-    @phase = :red
-    add_input_port :interrupt
-    add_output_port :observed
-  end
+  input :interrupt
+  output :observed
+
+  state_var phase : Symbol = :red
 
   def external_transition(messages)
     value = messages[input_ports[:interrupt]].first.as_sym
@@ -56,13 +53,10 @@ class TrafficLight < Quartz::AtomicModel
 end
 
 class Policeman < Quartz::AtomicModel
-  def initialize(name)
-    super(name)
-    @phase = :idle1
-    add_output_port :alternate
-    add_output_port :add_coupling
-    add_output_port :remove_coupling
-  end
+
+  state_var phase : Symbol = :idle1
+
+  output :alternate, :add_coupling, :remove_coupling
 
   def internal_transition
     @phase = case @phase

commit cb106c1f74cd642b5a14b18fa37c099d61f8edfc

fix bug caused by bd11d1d

src/quartz/atomic.cr
@@ -15,6 +15,7 @@ module Quartz
     def initialize(name, state)
       super(name)
       @bag = SimpleHash(OutputPort, Any).new
+      self.initial_state = state
       self.state = state
     end
 
@@ -25,8 +26,8 @@ module Quartz
         raise InvalidProcessorError.new("trying to initialize state of model \"#{name}\" from an invalid processor")
       end
 
-      if state = initial_state
-        self.state = state
+      if s = initial_state
+        self.state = s
       end
     end
 
@@ -44,7 +45,8 @@ module Quartz
         when "time"
           _time = SimulationTime.new(pull)
         when "state"
-          self.state = {{ (@type.name + "::State").id }}.new(pull)
+          self.initial_state = {{ (@type.name + "::State").id }}.new(pull)
+          self.state = initial_state
         else
           raise ::JSON::ParseException.new("Unknown json attribute: #{key}", 0, 0)
         end
@@ -68,7 +70,8 @@ module Quartz
         when "time".to_slice
           _time = SimulationTime.new(pull)
         when "state".to_slice
-          self.state = {{ (@type.name + "::State").id }}.new(pull)
+          self.initial_state = {{ (@type.name + "::State").id }}.new(pull)
+          self.state = initial_state
         else
           raise MessagePack::UnpackException.new("unknown msgpack attribute: #{String.new(key)}")
         end

src/quartz/multi_component/component.cr
@@ -18,6 +18,7 @@ module Quartz
 
       def initialize(name, state)
         super(name)
+        self.initial_state = state
         self.state = state
       end
 
@@ -28,8 +29,8 @@ module Quartz
           raise InvalidProcessorError.new("trying to initialize state of model \"#{name}\" from an invalid processor")
         end
 
-        if state = initial_state
-          self.state = state
+        if s = initial_state
+          self.state = s
         end
       end
 

src/quartz/state.cr
@@ -171,7 +171,7 @@ module Quartz
         \{% if @type.methods.any? { |m| m.name.stringify == "initialize" } %}
           \{% for method in @type.methods %}
             \{% if method.name == "initialize" %}
-              \{{ method.visibility.id }} def \{{ method.name.id }}(
+              def \{{ method.name.id }}(
                 \{% if method.splat_index.is_a?(NumberLiteral) %}
                   \{% for arg, index in method.args %}
                     \{{ (index == method.splat_index ? "*" + arg.stringify : arg.stringify).id }},
@@ -181,7 +181,7 @@ module Quartz
                 \{% end %}
                 \{{ (method.double_splat.is_a?(Nop) ? "" : "**" + method.double_splat.stringify).id }}
                 \{{ (method.block_arg.is_a?(Nop) ? "" : "&" + method.block_arg.stringify).id }}
-              ) \{{ (method.return_type.is_a?(Nop) ? "" : ":").id }} \{{ method.return_type }}
+              )
 
                 \{% for block in STATE_INITIALIZE %}
                   \{{ block }}
@@ -268,40 +268,9 @@ module Quartz
 
               \{% for var in STATE_VARIABLES %}
                 \{% if var[:value] == nil %}
-                  if value = args[:\{{ var[:name].id }}]? || args[\{{ var[:name].stringify }}]?
+                  if value = args[:\{{ var[:name].id }}]?
                     @\{{ var[:name].id }} = value
                   end
-                \{% end %}
-              \{% end %}
-            end
-
-            def initialize(
-              \{% for var in STATE_VARIABLES %}
-                \{% if var[:value] != nil %}
-                  \{% if var[:value].is_a?(Block) %}
-                    \{{ var[:name].id }} : \{{ var[:type] }}? = nil,
-                  \{% elsif var[:name] == nil %}
-                    \{{ var[:name].id }} : \{{ var[:type] }},
-                  \{% else %}
-                    \{{ var[:name].id }} : \{{ var[:type] }} = \{{ var[:value] }},
-                  \{% end %}
-                \{% end %}
-              \{% end %}
-            )
-              \{% for block in STATE_INITIALIZE %}
-                \{{ block }}
-              \{% end %}
-
-              \{% for var in STATE_VARIABLES %}
-                \{% if var[:value] != nil %}
-                  \{% if var[:value].is_a?(Block) %}
-                    if \{{ var[:name].id }}.nil?
-                      \{{ var[:name].id }} = (
-                        \{{ var[:value].body }}
-                      )
-                    end
-                  \{% end %}
-                  @\{{ var[:name].id }} = \{{ var[:name].id }}
                 \{% end %}
               \{% end %}
             end
@@ -549,11 +518,7 @@ module Quartz
           @_initial_state : Quartz::State?
 
           protected def initial_state
-            if state = @_initial_state
-              state.as(\{{@type.name.id}}::State)
-            else
-              nil
-            end
+            (@_initial_state || \{{@type.name.id}}::State.new).as(\{{@type.name.id}}::State)
           end
 
           def state
@@ -587,6 +552,8 @@ module Quartz
           \{% for x in STATE_VARIABLES %}
             \{% if x[:value].is_a?(Block) %}
               state_var \{{x[:name]}} : \{{x[:type]}}, visibility: \{{x[:visibility] || ""}} \{{ x[:value] }}
+            \{% elsif x[:value] == nil %}
+              state_var \{{x[:name]}} : \{{x[:type]}}, visibility: \{{x[:visibility] || "" }}
             \{% else %}
               state_var \{{x[:name]}} : \{{x[:type]}} = \{{ x[:value] }}, visibility: \{{x[:visibility] || "" }}
             \{% end %}
@@ -596,3 +563,4 @@ module Quartz
     end
   end
 end
+

spec/atomic_spec.cr
@@ -14,14 +14,23 @@ private class FetchOutputTest < AtomicModel
   end
 end
 
-private class ModelSample < AtomicModel
+class ModelSample < AtomicModel
   state_var x : Int32 = 0
   state_var y : Int32 = 0
 
   @sigma = 25
+
+  def evolve
+    @x = 100
+    @y = 254
+  end
 end
 
 private class MockProcessor < Processor
+  def initialize_processor(time)
+    0
+  end
+
   def collect_outputs(time)
     Hash(OutputPort, Any).new
   end
@@ -74,6 +83,22 @@ describe "AtomicModel" do
 
       m.x.should eq 5
       m.y.should eq 10
+    end
+
+    it "uses default state if no initial state is specified before running a simulation" do
+      m = ModelSample.new("foo")
+      m.x.should eq 0
+      m.y.should eq 0
+      p = MockProcessor.new(m)
+
+      m.evolve
+
+      m.x.should eq 100
+      m.y.should eq 254
+
+      m.__initialize_state__(p)
+      m.x.should eq 0
+      m.y.should eq 0
     end
 
     it "raises if wrong processor asks initialization" do
@@ -142,7 +167,7 @@ describe "AtomicModel" do
       m = ModelSample.new(MessagePack::Unpacker.new(io))
       m.name.should eq "foo"
       m.sigma.should eq 25
-      m.time.should eq 100
+      m.time.should eq 42
       m.x.should eq 5
       m.y.should eq 10
     end