RomainFranceschini/quartz

commit 4b8c3696fab72338b35d672edb3351ea16f406d4

refactor factory method

src/quartz/multi_component/simulator.cr
@@ -16,9 +16,9 @@ module Quartz
       def initialize(model, simulation)
         super(model, simulation)
         sched_type = model.class.preferred_event_set? || simulation.default_scheduler
-        @event_set = EventSetFactory(Component).new_event_set(sched_type)
         @state_bags = Hash(Quartz::MultiComponent::Component,Array(Tuple(Name,Any))).new { |h,k|
           h[k] = Array(Tuple(Name,Any)).new
+        @event_set = EventSet(Component).new(sched_type)
         }
         @imm = Array(Quartz::MultiComponent::Component).new
         @parent_bag = Hash(OutputPort, Array(Any)).new { |h, k|

src/quartz/coordinator.cr
@@ -12,7 +12,7 @@ module Quartz
 
       @children = Array(Processor).new
       scheduler_type = model.class.preferred_event_set? || simulation.default_scheduler
-      @scheduler = EventSetFactory(Processor).new_event_set(scheduler_type)
+      @scheduler = EventSet(Processor).new(scheduler_type)
       @synchronize = Array(SyncEntry).new
       @parent_bag = Hash(OutputPort, Array(Any)).new { |h, k|
         h[k] = Array(Any).new

src/quartz/schedulers.cr
@@ -5,4 +5,3 @@ require "./schedulers/ladder_queue"
 require "./schedulers/calendar_queue"
 # require "./schedulers/sorted_list"
 # require "./schedulers/minimal_list"
-require "./schedulers/event_set_factory"

src/quartz/schedulers/event_set_factory.cr
@@ -1,19 +1,0 @@
-module Quartz
-  module EventSetFactory(T)
-    def self.new_event_set(event_set) : EventSet(T)
-      case event_set
-      # when :binary_heap then BinaryHeap(T).new
-      # when :minimal_list then MinimalList(T).new
-      # when :sorted_list then SortedList(T).new
-      # when :splay_tree then SplayTree(T).new
-      when :ladder_queue   then LadderQueue(T).new
-      when :calendar_queue then CalendarQueue(T).new
-      else
-        if (logger = Quartz.logger?) && logger.warn?
-          logger.warn("Unknown event set '#{event_set}', defaults to calendar queue")
-        end
-        CalendarQueue(T).new
-      end
-    end
-  end
-end

src/quartz/schedulers/scheduler.cr
@@ -23,6 +23,17 @@ module Quartz
   # end
 
   abstract class EventSet(T) < PriorityQueue(T)
+    def self.new(event_set : Symbol) : self
+      case event_set
+      when :ladder_queue   then LadderQueue(T).new
+      when :calendar_queue then CalendarQueue(T).new
+      else
+        if (logger = Quartz.logger?) && logger.warn?
+          logger.warn("Unknown event set '#{event_set}', defaults to calendar queue")
+        end
+        CalendarQueue(T).new
+      end
+    end
 
     @[AlwaysInline]
     def next_priority : Priority

commit c0b9c741763fa721508a828b8efafbcdee5e4c67

fixed regression caused by 4b8c369

src/quartz/multi_component/simulator.cr
@@ -16,9 +16,9 @@ module Quartz
       def initialize(model, simulation)
         super(model, simulation)
         sched_type = model.class.preferred_event_set? || simulation.default_scheduler
+        @event_set = EventSet(Component).new(sched_type)
         @state_bags = Hash(Quartz::MultiComponent::Component,Array(Tuple(Name,Any))).new { |h,k|
           h[k] = Array(Tuple(Name,Any)).new
-        @event_set = EventSet(Component).new(sched_type)
         }
         @imm = Array(Quartz::MultiComponent::Component).new
         @parent_bag = Hash(OutputPort, Array(Any)).new { |h, k|