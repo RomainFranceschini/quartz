439d3b2466d46470bc19dcdbfbcd9b5ad3e559e3
Romain Franceschini <franceschini.romain@gmail.com>
2017-10-25 18:16:49 +0200

    time wrapper wip

--- spec/time_spec.cr
+++ spec/time_spec.cr
@@ -4,35 +4,160 @@ require "big"
 describe "VTime" do
   describe "comparison" do
     it "does with primitive types" do
-      (VTime.new(0.0) == VTime.new(0.0)).should be_true
-      (VTime.new(1.0) > VTime.new(0.0)).should be_true
-      (VTime.new(1.0) < VTime.new(0.0)).should be_false
+      (VirtualTime.new(0.0) == VirtualTime.new(0.0)).should be_true
+      (VirtualTime.new(1.0) > VirtualTime.new(0.0)).should be_true
+      (VirtualTime.new(1.0) < VirtualTime.new(0.0)).should be_false
     end
 
     it "does with arbitrary precision types" do
-      (VTime.new(BigFloat.new("0.99")) == VTime.new(BigFloat.new("0.99"))).should be_true
-      (VTime.new(BigFloat.new("0.99")) > VTime.new(BigInt.new("12"))).should be_false
-      (VTime.new(BigFloat.new("0.99")) < VTime.new(BigInt.new("12"))).should be_true
+      (VirtualTime.new(BigFloat.new("0.99")) == VirtualTime.new(BigFloat.new("0.99"))).should be_true
+      (VirtualTime.new(BigFloat.new("0.99")) > VirtualTime.new(BigInt.new("12"))).should be_false
+      (VirtualTime.new(BigFloat.new("0.99")) < VirtualTime.new(BigInt.new("12"))).should be_true
     end
   end
 
-  it "does addition" do
-    (VTime.new(1) + VTime.new(1)).should eq(VTime.new(2))
+  describe "arithmetics" do
+    it "does addition" do
+      (VirtualTime.new(1) + VirtualTime.new(1)).should eq(VirtualTime.new(2))
+    end
+
+    it "does substraction" do
+      (VirtualTime.new(2) - VirtualTime.new(1)).should eq(VirtualTime.new(1))
+    end
+
+    # it "does division" do
+    #   (VirtualTime(Int32).new(10) / VirtualTime(Int32).new(2)).should eq(VirtualTime(Int32).new(5))
+    # end
+
+    it "does multiplication" do
+      (VirtualTime.new(2) * VirtualTime.new(2)).should eq(VirtualTime.new(4))
+    end
+
+    it "does -" do
+      (-VirtualTime.new(2)).should eq(VirtualTime.new(-2))
+    end
   end
 
-  it "does substraction" do
-    (VTime.new(2) - VTime.new(1)).should eq(VTime.new(1))
+  describe "infinity" do
+    it "has specific infinity value" do
+      VirtualTime(Int32).infinity.should eq(VirtualTime(Float32).infinity)
+    end
+
+    it "has negative infinity" do
+      (-VirtualTime(Int32).infinity).should eq(VirtualTime(Float64).new(VirtualTime::Infinity.negative))
+    end
+
+    it "is equal to IEE-754 infinity values" do
+      (VirtualTime(Int32).infinity).should eq(Float32::INFINITY)
+      (VirtualTime(Int64).infinity).should eq(Float64::INFINITY)
+      (-VirtualTime(Int32).infinity).should eq(-Float32::INFINITY)
+      (-VirtualTime(Int32).infinity).should eq(-Float64::INFINITY)
+    end
+
+    it "support comparison" do
+      (VirtualTime::Infinity.new > Int64::MAX).should be_true
+      (-VirtualTime::Infinity.new < Int64::MIN).should be_true
+
+      (VirtualTime(Int64).infinity > VirtualTime.new(Int64::MAX)).should be_true
+      (-VirtualTime(Int64).infinity < VirtualTime.new(Int64::MIN)).should be_true
+      (VirtualTime(Int32).infinity < VirtualTime(Int32).zero).should be_false
+      (VirtualTime(Int32).infinity > VirtualTime(Int32).zero).should be_true
+    end
+
+    it "support arithmetic operators" do
+      (VirtualTime::Infinity.new + 1).should eq(VirtualTime::Infinity.new)
+      (VirtualTime::Infinity.new - 1.0).should eq(VirtualTime::Infinity.new)
+      (VirtualTime::Infinity.new * 2).should eq(VirtualTime::Infinity.new)
+      (VirtualTime::Infinity.new / 1i16).should eq(VirtualTime::Infinity.new)
+
+      (VirtualTime::Infinity.new + -4).should eq(VirtualTime::Infinity.new)
+      (VirtualTime::Infinity.new - -4).should eq(VirtualTime::Infinity.new)
+      (VirtualTime::Infinity.new * -2).should eq(-VirtualTime::Infinity.new)
+      (VirtualTime::Infinity.new / -2).should eq(-VirtualTime::Infinity.new)
+
+      (-VirtualTime::Infinity.new + 1).should eq(-VirtualTime::Infinity.new)
+      (-VirtualTime::Infinity.new - 1.0).should eq(-VirtualTime::Infinity.new)
+      (-VirtualTime::Infinity.new * 2).should eq(-VirtualTime::Infinity.new)
+      (-VirtualTime::Infinity.new / 4i16).should eq(-VirtualTime::Infinity.new)
+
+      (-VirtualTime::Infinity.new + -4).should eq(-VirtualTime::Infinity.new)
+      (-VirtualTime::Infinity.new - -4).should eq(-VirtualTime::Infinity.new)
+      (-VirtualTime::Infinity.new * -2).should eq(VirtualTime::Infinity.new)
+      (-VirtualTime::Infinity.new / -2).should eq(VirtualTime::Infinity.new)
+
+      (VirtualTime::Infinity.new + VirtualTime::Zero.new).should eq(VirtualTime::Infinity.new)
+    end
+
+    it "can be the rhs of a number operation" do
+      (1.0f32 + VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+      (2 - VirtualTime::Infinity.new).should eq(-VirtualTime::Infinity.new)
+      (4 * VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+      (10 / VirtualTime::Infinity.new).should eq(0)
+
+      (-1.0 + VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+      (-2 - VirtualTime::Infinity.new).should eq(-VirtualTime::Infinity.new)
+      (-5 * VirtualTime::Infinity.new).should eq(-VirtualTime::Infinity.new)
+      (-3 / VirtualTime::Infinity.new).should eq(0)
+
+      (1.0f32 + -VirtualTime::Infinity.new).should eq(-VirtualTime::Infinity.new)
+      (2 - -VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+      (4 * -VirtualTime::Infinity.new).should eq(-VirtualTime::Infinity.new)
+      (4 / -VirtualTime::Infinity.new).should eq(0)
+
+      (-1.0f32 + -VirtualTime::Infinity.new).should eq(-VirtualTime::Infinity.new)
+      (-2 - -VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+      (-4 * -VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+      (-4 / -VirtualTime::Infinity.new).should eq(0)
+
+      (VirtualTime::Zero.new + VirtualTime::Infinity.new).should eq(VirtualTime::Infinity.new)
+    end
   end
 
-  it "does division" do
-    (VTime.new(10) / VTime.new(2)).should eq(VTime.new(5))
-  end
+  describe "zero" do
+    it "has specific zero value" do
+      VirtualTime(Int32).zero.should eq(VirtualTime(Float64).new(0.0))
+    end
 
-  it "does multiplication" do
-    (VTime.new(2) * VTime.new(2)).should eq(VTime.new(4))
-  end
+    it "is equal to primitive number zero values" do
+      VirtualTime(Int32).zero.should eq(Int32.zero)
+      VirtualTime(Int32).zero.should eq(Int64.zero)
+      VirtualTime(Int32).zero.should eq(Int16.zero)
+      VirtualTime(Int32).zero.should eq(Int8.zero)
+      VirtualTime(Int32).zero.should eq(UInt32.zero)
+      VirtualTime(Int32).zero.should eq(UInt64.zero)
+      VirtualTime(Int32).zero.should eq(UInt16.zero)
+      VirtualTime(Int32).zero.should eq(UInt8.zero)
+      VirtualTime(Int32).zero.should eq(Float32.zero)
+      VirtualTime(Int32).zero.should eq(Float64.zero)
+    end
 
-  it "does -" do
-    (-VTime.new(2)).should eq(VTime.new(-2))
+    it "support comparison" do
+      (VirtualTime(Int32).zero < 1).should be_true
+      (VirtualTime(Int32).zero > 1.0).should be_false
+      (VirtualTime(Int32).zero < VirtualTime(Int32).infinity).should be_true
+      (VirtualTime(Int32).zero > VirtualTime(Int32).infinity).should be_false
+    end
+
+    it "support arithmetic operators" do
+      (VirtualTime::Zero.new + 1).should eq(1)
+      (VirtualTime::Zero.new - 1).should eq(-1)
+      (VirtualTime::Zero.new * 2).should eq(0)
+      (VirtualTime::Zero.new / 10).should eq(0)
+      (VirtualTime::Zero.new + -10).should eq(-10)
+      (VirtualTime::Zero.new - -10).should eq(10)
+    end
+
+    it "can be the rhs of number operations" do
+      (1 + VirtualTime::Zero.new).should eq(1)
+      (1 - VirtualTime::Zero.new).should eq(1)
+      (-1 + VirtualTime::Zero.new).should eq(-1)
+      (-1 - VirtualTime::Zero.new).should eq(-1)
+      (2 * VirtualTime::Zero.new).should eq(0)
+      expect_raises(DivisionByZero) do
+        (2 / VirtualTime::Zero.new)
+      end
+      (2.0 / VirtualTime::Zero.new).should eq(Float64::INFINITY)
+      (2.0 / VirtualTime::Zero.new).should eq(VirtualTime::Infinity.new)
+    end
   end
 end
--- src/quartz/time.cr
+++ src/quartz/time.cr
@@ -8,23 +8,209 @@ module Quartz
   #
   # `VTime` allows the modeler to choose the underlying virtual time
   # representation to simulate a model.
-  struct VTime(T)
-    include Comparable(self)
+  abstract struct VTime
+    # :nodoc:
+    struct Infinity
+    end
 
-    getter raw : T
+    # :nodoc:
+    struct Zero
+      include Comparable(Zero)
+      include Comparable(Number)
+      include Comparable(Infinity)
+
+      def -(other)
+        -other
+      end
+
+      def +(other)
+        other
+      end
+
+      def *(other)
+        self
+      end
+
+      def /(other)
+        self
+      end
+
+      def -
+        self
+      end
+
+      def <=>(other : Zero)
+        0
+      end
+
+      def <=>(other : Infinity)
+        other.positive? ? -1 : 1
+      end
+
+      def <=>(other)
+        if 0 == other
+          0
+        elsif 0 < other
+          -1
+        else
+          1
+        end
+      end
+    end
+
+    # :nodoc:
+    struct Infinity
+      include Comparable(Infinity)
+      include Comparable(Number)
+      include Comparable(Zero)
+
+      def self.positive
+        new(Sign::Positive)
+      end
+
+      def self.negative
+        new(Sign::Negative)
+      end
+
+      enum Sign
+        Positive
+        Negative
+      end
+
+      @sign : Sign
+
+      def initialize
+        @sign = Sign::Positive
+      end
+
+      def initialize(@sign : Sign)
+      end
+
+      def negative? : Bool
+        @sign.negative?
+      end
+
+      def positive? : Bool
+        @sign.positive?
+      end
+
+      def -(other) : Infinity
+        self
+      end
+
+      def +(other) : Infinity
+        self
+      end
+
+      def *(other) : Infinity
+        if other < 0
+          -self
+        else
+          self
+        end
+      end
+
+      def /(other) : Infinity
+        if other < 0
+          -self
+        else
+          self
+        end
+      end
+
+      def - : Infinity
+        if positive?
+          Infinity.new(Sign::Negative)
+        else
+          Infinity.new(Sign::Positive)
+        end
+      end
+
+      def <=>(other : Infinity)
+        if positive?
+          other.positive? ? 0 : 1
+        else
+          other.negative? ? 0 : -1
+        end
+      end
+
+      def <=>(other : Float32)
+        if positive?
+          other == Float32::INFINITY ? 0 : 1
+        else
+          other == -Float32::INFINITY ? 0 : -1
+        end
+      end
+
+      def <=>(other : Float64)
+        if positive?
+          other == Float64::INFINITY ? 0 : 1
+        else
+          other == -Float64::INFINITY ? 0 : -1
+        end
+      end
+
+      def <=>(other)
+        positive? ? 1 : -1
+      end
+    end
+  end
+
+  # TODO doc
+  struct VirtualTime(T) < VTime
+    include Comparable(VirtualTime)
+    include Comparable(Number)
+
+    # JSON.mapping(raw: T)
+
+    def self.zero
+      {% if T < Number %}
+        new({{T}}.zero)
+      {% else %}
+        new(Zero.new)
+      {% end %}
+    end
+
+    def self.infinity
+      new(Infinity.positive)
+    end
+
+    getter raw : T | Infinity | Zero
+
+    def initialize(@raw : Zero)
+    end
+
+    def initialize(@raw : Infinity)
+    end
 
     def initialize(@raw : T)
-      {% if !T.union? && T < Number::Primitive %}
+      {% if T < Infinity || T == Zero %}
+        # Support special infinity and zero values
+      {% elsif !T.union? && T < Number::Primitive %}
         # Support primitive numbers
       {% elsif !T.union? && T < Comparable && T.methods.map(&.name.stringify).includes?("+") && T.methods.map(&.name.stringify).includes?("-") && T.methods.map(&.name.stringify).includes?("*") && T.methods.map(&.name.stringify).includes?("/") %}
         # Support comparable types supporting addition, substraction, division and multiplication
       {% else %}
-        {{ raise "Can only create VTime with comparable types supporting numeric operators, not #{T}." }}
+        {{ raise "Can only create VirtualTime with comparable types supporting numeric operators, not #{T}." }}
       {% end %}
     end
 
+    def infinity?
+      @raw.is_a?(Infinity) || @raw == Float32::INFINITY || @raw == -Float32::INFINITY || @raw == Float64::INFINITY || @raw == -Float64::INFINITY
+    end
+
+    def zero?
+      @raw.is_a?(Zero) || @raw == 0
+    end
+
+    # :nodoc:
     def hash
       @raw.hash
+    end
+
+    # :nodoc:
+    def clone
+      self
     end
 
     # :nodoc:
@@ -98,47 +284,151 @@ module Quartz
     end
 
     # Returns the result of subtracting `self` and *other*'s raw object.
-    def -(other : VTime(T)) : VTime(T)
-      VTime.new(@raw - other.raw)
+    def -(other : VirtualTime) : VirtualTime(T)
+      VirtualTime(T).new(@raw - other.raw)
     end
 
     # Returns the result of subtracting the raw object and *other*.
-    def -(other : T) : VTime(T)
-      VTime.new(@raw - other)
+    def -(other : T) : VirtualTime(T)
+      VirtualTime(T).new(@raw - other)
     end
 
     # Returns the result of adding `self` and *other*'s raw object.
-    def +(other : VTime(T)) : VTime(T)
-      VTime.new(@raw + other.raw)
+    def +(other : VirtualTime) : VirtualTime(T)
+      VirtualTime(T).new(@raw + other.raw)
     end
 
     # Returns the result of adding the raw object and *other*.
-    def +(other : T) : VTime(T)
-      VTime.new(@raw + other)
+    def +(other : T) : VirtualTime(T)
+      VirtualTime(T).new(@raw + other)
     end
 
     # Returns the result of multiplying `self` and *other*'s raw object.
-    def *(other : VTime(T)) : VTime(T)
-      VTime.new(@raw * other.raw)
+    def *(other : VirtualTime) : VirtualTime(T)
+      VirtualTime(T).new(@raw * other.raw)
     end
 
     # Returns the result of multiplying the raw object and *other*.
-    def *(other : T) : VTime(T)
-      VTime.new(@raw * other)
+    def *(other : T) : VirtualTime(T)
+      VirtualTime(T).new(@raw * other)
     end
 
     # Returns the result of dividing `self` and *other*'s raw object.
-    def /(other : VTime(T)) : VTime(T)
-      VTime.new(@raw / other.raw)
+    def /(other : VirtualTime) : VirtualTime(T)
+      VirtualTime(T).new(@raw / other.raw)
     end
 
     # Returns the result of dividing the raw object and *other*.
-    def /(other : T) : VTime(T)
-      VTime.new(@raw / other)
+    def /(other : T) : VirtualTime(T)
+      VirtualTime(T).new(@raw / other)
     end
 
-    def - : VTime(T)
-      VTime.new(-@raw)
+    def - : VirtualTime(T)
+      VirtualTime(T).new(-@raw)
+    end
+
+    # # Returns the result of subtracting `self` and *other*'s raw object.
+    # def -(other : VTime) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw - other.raw)
+    # end
+
+    # # Returns the result of subtracting the raw object and *other*.
+    # def -(other : T) # : VirtualTime(T)
+    #   VirtualTime(T).new(@raw - other)
+    # end
+
+    # # Returns the result of adding `self` and *other*'s raw object.
+    # def +(other : VTime) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw + other.raw)
+    # end
+
+    # # Returns the result of adding the raw object and *other*.
+    # def +(other : T) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw + other)
+    # end
+
+    # # Returns the result of multiplying `self` and *other*'s raw object.
+    # def *(other : VTime) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw * other.raw)
+    # end
+
+    # # Returns the result of multiplying the raw object and *other*.
+    # def *(other : T) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw * other)
+    # end
+
+    # # Returns the result of dividing `self` and *other*'s raw object.
+    # def /(other : VTime) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw / other.raw)
+    # end
+
+    # # Returns the result of dividing the raw object and *other*.
+    # def /(other : T) : VirtualTime(T)
+    #   VirtualTime(T).new(@raw / other)
+    # end
+
+    # def - : VirtualTime(T)
+    #   VirtualTime(T).new(-@raw)
+    # end
+
+    def to_i
+      @raw.to_i
+    end
+
+    def to_f
+      @raw.to_f
     end
   end
+
+  struct ::Number
+    include Comparable(Quartz::VTime::Zero)
+    include Comparable(Quartz::VTime::Infinity)
+
+    def <=>(other : Quartz::VTime::Zero)
+      self <=> 0
+    end
+
+    def <=>(other : Quartz::VTime::Infinity)
+      -(other <=> self)
+    end
+
+    def +(other : Quartz::VTime::Zero)
+      self
+    end
+
+    def -(other : Quartz::VTime::Zero)
+      self
+    end
+
+    def *(other : Quartz::VTime::Zero)
+      other
+    end
+
+    def /(other : Quartz::VTime::Zero)
+      self / 0
+    end
+
+    def +(other : Quartz::VTime::Infinity)
+      other
+    end
+
+    def -(other : Quartz::VTime::Infinity)
+      -other
+    end
+
+    def *(other : Quartz::VTime::Infinity)
+      if self < 0
+        -other
+      else
+        other
+      end
+    end
+
+    def /(other : Quartz::VTime::Infinity)
+      Quartz::VirtualTime.new(self.class.zero)
+    end
+  end
+
+  alias FloatVTime = VirtualTime(Float64)
+  alias IntVTime = VirtualTime(Int64)
 end
