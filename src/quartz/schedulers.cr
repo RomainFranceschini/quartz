608423ac3558bbc55e49ddbb71905cd90b146300
Romain Franceschini <franceschini.romain@gmail.com>
2018-04-23 18:44:56 +0200

    adapt binary heap to new priority queue interface

--- src/quartz/schedulers.cr
+++ src/quartz/schedulers.cr
@@ -1,8 +1,3 @@
-require "./schedulers/scheduler"
-#require "./schedulers/binary_heap"
-#require "./schedulers/splay_tree"
 require "./schedulers/binary_heap"
 require "./schedulers/ladder_queue"
 require "./schedulers/calendar_queue"
-# require "./schedulers/sorted_list"
-# require "./schedulers/minimal_list"
