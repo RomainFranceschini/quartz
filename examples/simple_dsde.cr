4912ce0c888fdec0cfd3bef5298ef6478af64004
Romain Franceschini <franceschini.romain@gmail.com>
2019-01-09 10:33:06 +0100

    fix simple dsde example

--- examples/simple_dsde.cr
+++ examples/simple_dsde.cr
@@ -31,9 +31,9 @@ class BirthController < Quartz::DSDE::Executive
 
   def time_advance
     case phase
-    when :init  then Quartz.duration(1)
-    when :death then Quartz.duration(5)
-    else             Quartz::Duration::INFINITY
+    when :init          then Quartz.duration(1)
+    when :death, :birth then Quartz.duration(5)
+    else                     Quartz::Duration::INFINITY
     end
   end
 
@@ -42,13 +42,16 @@ class BirthController < Quartz::DSDE::Executive
       remove_coupling_from_network(:out, from: :in, between: "model_0", and: "model_#{@counter}")
       remove_model_from_network("model_#{@counter}")
       @counter -= 1
+      @phase = :idle
     else
       add_model_to_network(OneTimeModel.new("model_#{@counter}"))
       add_coupling_to_network(:out, to: :in, between: "model_0", and: "model_#{@counter}") if @counter > 0
-      if @counter != 2
-        @counter += 1
-      end
-      @phase = :death
+      @phase = if @counter == 2
+                 :death
+               else
+                 @counter += 1
+                 :birth
+               end
     end
   end
 
@@ -73,9 +76,11 @@ class Grapher
   end
 end
 
+Quartz.logger.level = Logger::DEBUG
+
 model = Quartz::DSDE::CoupledModel.new(:dsde, BirthController.new(:executive))
 
-simulation = Quartz::Simulation.new(model, duration: Quartz.duration(25))
+simulation = Quartz::Simulation.new(model, duration: Quartz.duration(25), maintain_hierarchy: true)
 simulation.generate_graph("dsde_0")
 Grapher.new(model.executive, simulation)
 simulation.simulate
